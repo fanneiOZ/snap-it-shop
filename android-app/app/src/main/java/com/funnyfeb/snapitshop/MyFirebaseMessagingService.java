dbd4908c254b9a1c0510634c9a48fa7c29ae6245
Panya Batphantana <fanneioz@yahoo.co.th>
2022-05-20 22:20:04 +0700

    Firebase Messaging Service

---
+++ android-app/app/src/main/java/com/funnyfeb/snapitshop/MyFirebaseMessagingService.java
@@ -0,0 +1,15 @@
+package com.funnyfeb.snapitshop;
+
+import android.util.Log;
+
+import androidx.annotation.NonNull;
+
+import com.google.firebase.messaging.FirebaseMessagingService;
+
+public class MyFirebaseMessagingService extends FirebaseMessagingService {
+    @Override
+    public void onNewToken(@NonNull String token) {
+        super.onNewToken(token);
+        Log.d("TOKEN", "Refreshed token: " + token);
+    }
+}
